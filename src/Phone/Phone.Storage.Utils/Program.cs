37e572f499c266041c55799dbfe25e1621ec7e96
Pradeep S <pradeep.ansh.sumit@gmail.com>
2018-08-11 20:32:30 +0700

    first commit to empty repository

---
+++ src/Phone/Phone.Storage.Utils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phone.Storage.Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
